fae1421bea890ef75fb60e2ce4176f6e4a0bb504
Cristian <cristian.ciortea@student.unitbv.ro>
2023-03-28 10:47:04 +0300

    Added Mapping Extensions classes for Car and Salesman

---
+++ Dealership/DataLayer/Mapping/CarMappingExtensions.cs
@@ -0,0 +1,30 @@
+﻿using DataLayer.Dtos;
+using DataLayer.Entities;
+
+namespace DataLayer.Mapping
+{
+    public static class CarMappingExtensions
+    {
+        public static List<CarDto> ToCarDtos(this List<Car> cars)
+        {
+            var results = cars.Select(e => e.ToCarDto()).ToList();
+            return results;
+        }
+
+        public static CarDto ToCarDto(this Car car)
+        {
+            if (car == null) return null;
+
+            var result = new CarDto();
+            result.Year = car.Year;
+            result.Make = car.Make;
+            result.Mileage = car.Mileage;
+            result.FuelType = car.FuelType;
+            result.EngineSize = car.EngineSize;
+            result.Color = car.Color;
+            result.Model = car.Model;
+
+            return result;
+        }
+    }
+}
---
+++ Dealership/DataLayer/Mapping/SalesmanMappingExtensions.cs
@@ -0,0 +1,27 @@
+﻿using DataLayer.Dtos;
+
+namespace DataLayer.Mapping
+{
+    public static class SalesmanMappingExtensions
+    {
+        public static List<SalesmanDto> ToSalesmanDtos(this List<SalesmanDto> salesmen)
+        {
+            var results = salesmen.Select(e => e.ToSalesmanDto()).ToList();
+
+            return results;
+        }
+
+        public static SalesmanDto ToSalesmanDto(this SalesmanDto salesmanDto)
+        {
+            if (salesmanDto == null) return null;
+
+            var result = new SalesmanDto();
+            result.FirstName = salesmanDto.FirstName;
+            result.LastName = salesmanDto.LastName;
+            result.DateOfBirth = salesmanDto.DateOfBirth;
+            result.Address = salesmanDto.Address;
+
+            return result;
+        }
+    }
+}
